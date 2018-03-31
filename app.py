01d8238ebf9540d75333fb791ef501d3e3ffa5c2
Min Kim <min.c.kim@gmail.com>
2018-03-31 16:49:07 -0500

    More work on flask app, created a couple of intro routes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # import necessary libraries
 # import pandas as pd
-
+from credentials import user, password
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
@@ -59,7 +59,52 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+# route queries and returns a list of the country names. will be used to populate dropdown
+@app.route("/countries")
+def get_countries():
+    results = session.query(loans.country_name)
+    countries = []
+    for a in results:
+        countries.append(a.country_name)
+    countries = set(countries)
+    clean = []
+    for a in countries:
+        clean.append(a)
+    
+    return jsonify(clean)
+
+# Get country info
+@app.route("/countries/<country>")
+def get_country_info(country):
+    country = country.capitalize()
+    results = session.query(loans).\
+        filter(loans.country_name == country).all()
+    full_dict = []
+    for result in results:
+        current_dict = {}
+        current_dict["name"] = result.country_name
+        current_dict["country_code"] = result.country_code
+        current_dict["loan_id"] = result.loan_id
+        current_dict["status"] = result.status
+        current_dict["sector"] = result.sector_name
+        current_dict["activity"] = result.activity_name
+        current_dict["posted"] = result.posted_time
+        current_dict["disbursed"] = result.disburse_time
+        current_dict["raised"] = result.raised_time
+        current_dict["term"] = result.lender_term
+        current_dict["total_lenders"] = result.num_lenders_total
+        current_dict["interval"] = result.repayment_interval
+        current_dict["female"] = result.female_count
+        current_dict["male"] = result.male_count
+        current_dict["borrowers"] = result.borrower_count
+        current_dict["funded_amount"] = result.funded_amount_usd
+        current_dict["loan_amount"] = result.loan_amount_usd
+        current_dict["shortage_amount"] = result.shortage_fund
+        full_dict.append(current_dict)
+    
+    return jsonify(full_dict)
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
