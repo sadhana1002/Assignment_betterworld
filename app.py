8560b130d55d4d44cdc7db71767c153ee65915d2
Min Kim <min.c.kim@gmail.com>
2018-03-31 15:26:59 -0500

    added some code to flask app, code to create db

--- app.py
+++ app.py
@@ -6,25 +6,47 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from sqlalchemy import inspect
 
+# PyMySQL 
+import pymysql
+
+# Create Engine and Pass in MySQL Connection
+engine = create_engine(f"mysql://{user}:{password}@localhost:3306/kiva")
+
+#################################################
+# Database Setup
+#################################################
+
+
+# Create Base
+Base = automap_base()
+Base.prepare(engine, reflect = True)
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+conn = engine.connect()
+
+# Set tables
+
+loans = Base.classes.loans
+country = Base.classes.country
+
+# Create empty list to get column names for querying
+
+column_list_loans = []
+column_list_country = []
+for column in loans.__table__.columns:
+    column_list_loans.append(column.key)
+for column in country.__table__.columns:
+    column_list_country.append(column.key)
+
+print(column_list_country)
+print(column_list_loans)
+
 
 from flask import (
     Flask,
     render_template,
     jsonify)
-
-#################################################
-# Database Setup
-#################################################
-# engine = create_engine("database_goes_here")
-
-# reflect an existing database into a new model
-# Base = automap_base()
-# reflect the tables
-# Base.prepare(engine, reflect=True)
-
-# Create our session (link) from Python to the DB
-# session = Session(engine)
-
 
 
 #################################################
