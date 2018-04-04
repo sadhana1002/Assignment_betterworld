c9dc6e4a375ee190ba6ab770521d265722eed899
Min Kim <min.c.kim@gmail.com>
2018-04-04 09:27:49 -0500

    Added to map

--- static/js/main.js
+++ static/js/main.js
@@ -9,43 +9,45 @@ var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v1
 
 var url = "/countries";
 var geoURL = "https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json";
-d3.json(geoURL, function(error, geoResponse){
-    L.geoJson(geoResponse,{
-        style: function(feature) {
-            return {
-            fillOpacity: 0,
-            weight: 1.5
-            };
-        },
-        onEachFeature: function(feature, layer) {
-            // Set mouse events to change map styling
-            layer.on({
-            // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
-            mouseover: function(event) {
-                layer = event.target;
-                layer.setStyle({
-                fillOpacity: 0.5
-                });
+d3.json(url, function(error, response){
+    if(error) console.warn(error);   
+    d3.json(geoURL, function(error, geoResponse){
+        L.geoJson(geoResponse,{
+            style: function(feature) {
+                return {
+                fillOpacity: 0,
+                weight: 1.5
+                };
             },
-            // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
-            mouseout: function(event) {
-                layer = event.target;
-                layer.setStyle({
-                fillOpacity: 0
-                });
-            },
-            // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
-            click: function(event) {
-                map.fitBounds(event.target.getBounds());
-                var currentClickedCountry = event.target.feature.properties.name
-                createGraph(currentClickedCountry);
+            onEachFeature: function(feature, layer) {
+                // Set mouse events to change map styling
+                layer.on({
+                // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
+                mouseover: function(event) {
+                    layer = event.target;
+                    layer.setStyle({
+                    fillOpacity: 0.5
+                    });
+                },
+                // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
+                mouseout: function(event) {
+                    layer = event.target;
+                    layer.setStyle({
+                    fillOpacity: 0
+                    });
+                },
+                // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
+                click: function(event) {
+                    map.fitBounds(event.target.getBounds());
+                    var currentClickedCountry = event.target.feature.properties.name
+                    createGraph(currentClickedCountry);
+                }
+                });                
             }
-            });                
-        }
-    }).addTo(map);   
-});
-
-function createGraph(country){
+        }).addTo(map);   
+    });
+}); 
+function createGraph(country){ß
     console.log(country);
     if(country === "Laos"){
         var renameCountry = "Lao People's Democratic Republic";
@@ -63,7 +65,7 @@ function createGraph(country){
         if(error) console.warn(error);
         console.log(response)
     }); 
-} else if(country==="Democratic Republic of the Congo"){
+    } else if(country==="Democratic Republic of the Congo"){
     var renameCountry = "The Democratic Republic of the Congo";
     var url = `/countries/${renameCountry}`;
     console.log(url);
