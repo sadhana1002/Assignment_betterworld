ca2a7c52e17447654fa096cfb01e7f8b41e03d15
Min Kim <min.c.kim@gmail.com>
2018-04-05 21:39:14 -0500

    Subha and min complete

--- app.py
+++ app.py
@@ -156,7 +156,117 @@ def get_stacked_bar():
         empty.append(l)
     return jsonify(empty)
     
+@app.route("/genderwise_popular_sector")
+def sector_popularity():
     
+    sel = [func.sum(loans.female_count).label('female_borrower_count'), func.sum(loans.male_count).label('male_borrower_count'), loans.sector_name,loans.country_name]
+    results = session.query(*sel).\
+        group_by(loans.country_name,loans.sector_name).all()
+
+    # print(results)
+
+    master_list_sector = {}
+
+    for result in results:
+        sector_data = {}
+        sector_data['Female'] = int(result[0])
+        sector_data['Male'] = int(result[1])
+        sector_data['Sector'] = (result[2])
+
+        if  result[3] in master_list_sector:
+            master_list_sector
+            # print ("result[3]" + result[3])
+            master_list_sector[result[3]].append(sector_data)
+        else :
+            
+            master_list_sector[result[3]] = [sector_data]
+        
+
+    return jsonify(master_list_sector)
+
+@app.route("/topCountries")
+def topCountries():
+    sel = [func.count(loans.loan_id), loans.country_name]
+    results = session.query(*sel).\
+        group_by(loans.country_name).order_by(func.count(loans.loan_id).desc()).all()
+
+    # print(results)
+    top_3_Countries = []
+    for result in results:
+        country_details = {}
+        country_details["Country"] = (result[1])
+        country_details["NumberOfLoans"] = int(result[0])
+        top_3_Countries.append(country_details)
+
+    return jsonify(top_3_Countries)
+
+@app.route("/topCountriesByLoanCount")
+def topCountry():
+    return render_template("topCountry.html")
+
+
+@app.route("/philippinesData")
+def philippinesData():
+    sel = session.query(loans.sector_name, loans.activity_name, loans.loan_amount_usd, loans.country_name, func.count(loans.loan_id), loans.num_lenders_total).filter(loans.country_name == "Philippines").\
+    group_by(loans.sector_name).all()
+   
+    return jsonify(sel)
+
+
+
+@app.route("/kenyaData")
+def kenyaData():
+    sel = session.query(loans.sector_name, loans.activity_name, loans.loan_amount_usd, loans.country_name, func.count(loans.loan_id), loans.num_lenders_total).filter(loans.country_name == "Kenya").\
+    group_by(loans.sector_name).all()
+   
+    return jsonify(sel)
+
+
+
+@app.route("/peruData")
+def peruData():
+    sel = session.query(loans.sector_name, loans.activity_name, loans.loan_amount_usd, loans.country_name, func.count(loans.loan_id), loans.num_lenders_total).filter(loans.country_name == "Peru").\
+    group_by(loans.sector_name).all()
+   
+    return jsonify(sel)   
+
+@app.route("/gender_growth_over_years")
+def gender_count2():
+    # year_list = []
+    # year = session.query(func.extract('year', Loans.posted_time).distinct())
+    # for years in year:
+    #     year_list.append(years)
+    # print(year_list)
+    sel = [func.sum(loans.female_count).label('female_borrower_count'), func.sum(loans.male_count).label('male_borrower_count'), func.extract("year", loans.posted_time), loans.country_name]
+    results = session.query(*sel).\
+        group_by(loans.country_name,(func.extract("year", loans.posted_time))).all()
+
+    # print(results)   
+    
+
+    master_list = {}
+
+    for result in results:
+        gender_yeardata = {}
+        
+        gender_yeardata['YEAR'] = result[2]
+        gender_yeardata['FEMALE'] = int(result[0])
+        gender_yeardata['MALE'] = int(result[1])
+        #gender_count_year.append(gender_yeardata)
+
+        if  result[3]  in master_list:
+            master_list
+            # print ("result[3]" + result[3])
+            master_list[result[3]].append(gender_yeardata)
+        else :
+            
+            master_list[result[3]] = [gender_yeardata]
+            # print ( master_list )
+
+
+    return jsonify(master_list)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
